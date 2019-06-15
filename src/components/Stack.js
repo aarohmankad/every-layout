405c2f47467b0a2b86b1863b9321c16155b03b01
Aaroh Mankad <aarohmankad@gmail.com>
2019-06-15 13:43:54 -0700

    initial commit with Stack layout

---
+++ src/components/Stack.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import styled from 'styled-components';
+
+/*
+Example:
+<Stack space="1.5rem">
+  <h2>Header</h2>
+  <img src={logo} className="App-logo" />
+  <p>Paragraph</p>
+</Stack>;
+*/
+
+const StyledStack = styled.div`
+  --space: ${props => props.space || '1rem'};
+
+  & > * {
+    margin: 0;
+  }
+
+  & > * + * {
+    margin-top: var(--space);
+  }
+`;
+
+const Stack = props => <StyledStack {...props}>{props.children}</StyledStack>;
+
+export default Stack;
